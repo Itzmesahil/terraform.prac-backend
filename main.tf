07d82366a94b852da088c2d09960ba5516b716a8
Sahil <itzmesahilshaikh@gmail.com>
2024-07-04 10:53:04 +0530

    first task executed

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "example" {
-  ami           = ""
-  instance_type = "t2.micro"
+  ami           = var.ami_id
+  instance_type = var.instance_type
 
 }
 
--- variable.tf
+++ variable.tf
@@ -1,8 +1,10 @@
 variable "ami_id" {
   description = "This required ami id"
+  default     = "ami-01376101673c89611"
 }
 
 variable "instance_type" {
   description = "this required instance size"
+  default     = "t2.micro"
 
 }
